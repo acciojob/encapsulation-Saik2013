273dd00565e5738b06ae7a3585ff5d764c8b6f4d
Saikumar Ketireddi <kethireddysaikumar7@gmail.com>
2024-05-02 15:28:48 +0530

    code check

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[]args){
+        //object creating for RWonly class
+        RWOnly obj = new RWOnly();
+        //task-4: java: error-name has private access in com.driver.RWOnly
+        //obj.name = "Sai";
+        //System.out.println(obj.name);
+        obj.setName("sai");
+        System.out.println(obj.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    //getter method
+    public String getName(){
+        return name;
+    }
+    //setter method
+    public void setName(String name){
+        this.name = name;
+    }
 }
